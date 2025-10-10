5cdfd4408d91ea9aaaef97f288a3f4fa3e112803
RomanOglan <oglanroman@gmail.com>
2025-10-10 14:17:24 -0400

    Completed Assignment; Fixed TemptoColor formula; may come back and make it more readable.

--- simulation.cpp
+++ simulation.cpp
@@ -11,34 +11,27 @@ using namespace cv;
 std::array<double, 3> TempaturetoColor(double temp) {
     double minTemp = -20;
     double maxTemp = 100;
-    double midTemp = (100 + -20) / 2;
+    double midTemp = 40;
 
     double Blue;
     double Green;
     double Red;
 
     double ratio = 0;
-    if(temp < midTemp){
+    if(temp <= midTemp){
         ratio = (temp - minTemp) / (midTemp - minTemp); 
-        Blue = 0;
-        Green = 255 * (1 - ratio); 
-        Red   = ; 
-    }
-    else if(temp == midTemp){
-        Blue = 255;
+        Blue = 255 * ratio;
         Green = 255;
-        Red = 255;
-        
+        Red   = 255 * ratio; 
     }
     else{
         ratio = (temp - midTemp) / (maxTemp - midTemp);
-        Blue = 0;
-        Green = 0;
-        Red = 255 * (ratio);
+        Blue = 255 * (1 - ratio);
+        Green = 255 * (1 - ratio);
+        Red = 255;
     }
     
     std::array<double, 3> color = {Blue, Green, Red};
-
     return color;
 }
 
@@ -46,7 +39,7 @@ int main(int argc, char* argv[]) {
     int M, N;
     Mat img;
 
-    double lowestDiff = 0;
+    double highestDiff = 1;
     double currentDiff = 0;
 
     if (argc < 3) {
@@ -60,16 +53,14 @@ int main(int argc, char* argv[]) {
     N = atoi(argv[2]);
 
     std::vector<std::vector<double>> temp(M, std::vector<double>(N, -20.0)); //creating tempature vector; temp
-    
-    
     img = Mat(M, N, CV_8UC3, Scalar(255, 255, 255)); //creating imagel img
 
-    //initiating start state
+    //drawing start state
     for(int i = 0; i < M; i++){
         for(int j = 0; j < N; j++){
-            if(i < 1 || i > 254 || j < 1 || j > 254){
-                
-                if(i < 254 && 120 <= j && j <= 136){
+
+            if(i==0 || i == M - 1 || j ==0 || j ==  N - 1 ){
+                if (i == 0 && (j >= (N / 2 - 5) && j <= (N / 2 + 5))) {
                     temp[i][j] = 100;
                 }
                 else{
@@ -88,33 +79,38 @@ int main(int argc, char* argv[]) {
         }
 
     }
-    int num = 0;
-
+    
     imshow("color display", img);
     printf("Press any key to terminate the program.\n");
 
-    //added this as pressing a key to terminate was inconsistent on mac
+
     while (true) {
         int key = waitKey(1);
         if (key >= 0) break; 
+        if (highestDiff < 0.01) break;
+        highestDiff = 0;
 
         std::vector<std::vector<double>> newTemp = temp;
         
         for (int i = 1; i < M - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
-                newTemp[i][j] = (temp[i+1][j] + temp[i-1][j] + temp[i][j+1] + temp[i][j-1]) / 4;
-                
-                }
+                if (i == M - 1 && (j >= (N / 2 - 5) && j <= (N / 2 + 5))) continue; //making sure not to change tempature of heater
 
+                newTemp[i][j] = (temp[i+1][j] + temp[i-1][j] + temp[i][j+1] + temp[i][j-1]) / 4.0;
+                
+                currentDiff = abs(newTemp[i][j] - temp[i][j]);
+                if(currentDiff > highestDiff) highestDiff = currentDiff;
+                
             }
-        
+
+        }
         
         temp = newTemp;
     
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 auto colors = TempaturetoColor(temp[i][j]);
-                img.data[i * img.step[0] + j * img.step[1]     ] = 0;
+                img.data[i * img.step[0] + j * img.step[1]     ] = colors[0];
                 img.data[i * img.step[0] + j * img.step[1] + 1 ] = colors[1];
                 img.data[i * img.step[0] + j * img.step[1] + 2 ] = colors[2];
             }
